37234e3638d256f80cd4ec9ea177e34b915d86f1
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-03-19 20:47:11 -0700

    Change order of env variables

--- src/env.mjs
+++ src/env.mjs
@@ -5,8 +5,8 @@ import { z } from "zod";
  * built with invalid env vars.
  */
 const server = z.object({
-  DATABASE_URL: z.string().url(),
   DATABASE: z.string(),
+  DATABASE_URL: z.string().url(),
   DATABASE_USERNAME: z.string(),
   DATABASE_PASSWORD: z.string(),
   NODE_ENV: z.enum(["development", "test", "production"]),
@@ -27,8 +27,8 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
-  DATABASE_URL: process.env.DATABASE_URL,
   DATABASE: process.env.DATABASE,
+  DATABASE_URL: process.env.DATABASE_URL,
   DATABASE_USERNAME: process.env.DATABASE_USERNAME,
   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
   NODE_ENV: process.env.NODE_ENV,

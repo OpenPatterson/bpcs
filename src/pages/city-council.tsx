59cf76282836aac09138590f5b515ba40c7c0d4e
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-03-28 12:35:17 -0700

    District A Zone in Map

--- src/pages/city-council.tsx
+++ src/pages/city-council.tsx
@@ -1,9 +1,22 @@
 import { type NextPage } from "next";
+import Head from "next/head";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import dynamic from "next/dynamic";
+
 
 const CityCouncil: NextPage = () => {
-    return (
-        <div className="3xl">City Council</div>
-    )
-}
+    const MapWithNoSSR = dynamic(() => import("~/components/map"), {
+        ssr: false
+      });
+  return (
+    <>
+      <main>
+        <div className="h-600 w-100" id="map">
+          <MapWithNoSSR />
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default CityCouncil;

17cc7caf30921092f17d8edc840aeae9fea4d5e6
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-04-02 22:53:38 -0700

    Adding analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Script from "next/script";
 import { Navbar } from "~/components/navbar";
 
 import "~/styles/globals.css";
@@ -6,6 +7,7 @@ import "~/styles/globals.css";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
   <>
+  <Script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async defer />
     <Navbar />
     <Component {...pageProps} />
   </>

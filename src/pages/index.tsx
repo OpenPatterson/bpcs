2428823764ba173c99491618b2fcb53adcbcf8e3
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-04-07 00:51:06 -0700

    Fixed some styling on index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -137,51 +137,91 @@ const Home: NextPage<HomeProps> = ({ queriedMeetings, queryUpcoming }) => {
       </Head>
       <div className="">
         <div className="my-5 text-center">
-          <h1 className="my-5 text-7xl font-bold">Next Meeting</h1>
+          <h1 className="my-5 font-bold text-5xl md:text-7xl 2xl:text-9xl">Next Meeting</h1>
           <div>
             <Link
               className="flex-column"
               href={`/meetings/${queryUpcoming.id}`}
               key={queryUpcoming.id}
             >
-              <h1 className="text-2xl">{queryUpcoming.meetingType}</h1>
-              <h1 className="text-2xl">{queryUpcoming.meetingTime}</h1>
+              <div className="text-2xl hover:underline flex flex-col">
+                <div className="">{queryUpcoming.meetingType}</div>
+                <div className="">{queryUpcoming.meetingTime}</div>
+              </div>
+              <div className="group relative mx-auto flex w-10 justify-center">
+                {/* Solid */}
+                <div className="hidden group-hover:block">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="h-10 w-10"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <div className="group-hover:hidden">
+                  {/* Outline */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="h-10 w-10 2xl:h-20 2xl:w-20"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </div>
+              </div>
             </Link>
           </div>
         </div>
+        {/* Previous Meetings Title */}
         <div className="max-w-full bg-primary-light">
           <Link href={"/meetings"} className="">
-            <div className="mx-auto w-2/3 text-3xl font-bold">
+            <div className="mx-auto w-3/4 text-3xl font-bold">
               Previous Meetings
             </div>
           </Link>
-          <div className="mx-auto w-2/3 text-sm">Last Updated:</div>
+          <div className="mx-auto w-3/4 text-sm">Last Updated:</div>
         </div>
-        <div>
+        {/* 10 Previous Meetings */}
+        <div className="">
           {queriedMeetings.map((meeting, index) => (
             <div
               key={meeting.id}
               className={index % 2 == 0 ? "" : "bg-primary-light"}
             >
-              <div className="mx-auto flex w-2/3 flex-row items-center justify-between">
+              <div className="w-2/3 mx-auto md:mx-auto md:items-center md:justify-between md:w-3/4 md:flex md:flex-row">
                 <Link
                   className=""
                   href={`/meetings/${meeting.id}`}
                   key={meeting.id}
                 >
-                  <div className="flex flex-row items-center text-left">
-                    <div className="flex flex-col">
-                      <div className="">{meeting.meetingType}</div>
-                      <div className="">{meeting.meetingTime}</div>
+                  <div className="flex items-center text-left">
+                    <div className="">
+                      <div className="hover:underline">
+                        <div className="">{meeting.meetingType}</div>
+                        <div className="">{meeting.meetingTime}</div>
+                      </div>
                     </div>
-                    <div className="group relative flex">
-                      {/* Solid */}
+                    <div className="group">
+                      {/* Solid SVG */}
                       <div className="hidden group-hover:block">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 24 24"
                           fill="currentColor"
-                          className="h-6 w-6"
+                          className="h-6 w-6 2xl:h-10 2xl:w-10"
                         >
                           <path
                             fill-rule="evenodd"
@@ -191,14 +231,14 @@ const Home: NextPage<HomeProps> = ({ queriedMeetings, queryUpcoming }) => {
                         </svg>
                       </div>
                       <div className="group-hover:hidden">
-                        {/* Outline */}
+                        {/* Outline SVG */}
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
                           viewBox="0 0 24 24"
                           stroke-width="1.5"
                           stroke="currentColor"
-                          className="h-6 w-6"
+                          className="h-6 w-6 2xl:h-10 2xl:w-10"
                         >
                           <path
                             stroke-linecap="round"
@@ -210,14 +250,14 @@ const Home: NextPage<HomeProps> = ({ queriedMeetings, queryUpcoming }) => {
                     </div>
                   </div>
                 </Link>
-                <div className="flex flex-row space-x-5">
+                <div className="space-x-3">
                   {meeting.agendaURL == "" ? null : (
                     <Link
                       href={
                         "https://pattersonca.iqm2.com/Citizens/" +
                         meeting.agendaURL
                       }
-                      className="flex"
+                      className="hover:underline"
                       target="_blank"
                     >
                       Agenda
@@ -229,7 +269,7 @@ const Home: NextPage<HomeProps> = ({ queriedMeetings, queryUpcoming }) => {
                         "https://pattersonca.iqm2.com/Citizens/" +
                         meeting.agendaPacketURL
                       }
-                      className="flex"
+                      className="hover:underline"
                       target="_blank"
                     >
                       Agenda Packet
@@ -241,7 +281,7 @@ const Home: NextPage<HomeProps> = ({ queriedMeetings, queryUpcoming }) => {
                         "https://pattersonca.iqm2.com/Citizens/" +
                         meeting.minutesURL
                       }
-                      className="flex"
+                      className="hover:underline"
                       target="_blank"
                     >
                       Minutes
@@ -253,7 +293,7 @@ const Home: NextPage<HomeProps> = ({ queriedMeetings, queryUpcoming }) => {
                         "https://pattersonca.iqm2.com/Citizens/" +
                         meeting.summaryURL
                       }
-                      className="flex"
+                      className="hover:underline"
                       target="_blank"
                     >
                       Summary

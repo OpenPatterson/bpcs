8b0bf88776bb21715914e379781ec032eed27966
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-04-07 11:19:42 -0700

    17 pretty ify front page (#37)
    * Changed format of dates on index
    * On hover arrows by meetings
    * Fixed type issues in index
    * Fixed some styling on index
    * Finalized current version of styling for front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,18 +10,56 @@ type HomeProps = {
   queriedMeetings: {
     id: number;
     meetingTime: string;
+    meetingType: string;
+    agendaURL: string;
+    agendaPacketURL: string;
+    summaryURL: string;
+    minutesURL: string;
   }[];
   queryUpcoming: {
     id: number;
     meetingTime: string;
+    meetingType: string;
+    agendaURL: string;
+    agendaPacketURL: string;
+    summaryURL: string;
+    minutesURL: string;
   };
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const queriedMeetings: { id: number; meetingTime: string }[] = [];
-  const queryUpcoming: { id: number; meetingTime: string } = {
+  const dateOptions: Record<string, string | undefined> = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+  const queriedMeetings: {
+    id: number;
+    meetingTime: string;
+    meetingType: string;
+    agendaURL: string;
+    agendaPacketURL: string;
+    summaryURL: string;
+    minutesURL: string;
+  }[] = [];
+  const queryUpcoming: {
+    id: number;
+    meetingTime: string;
+    meetingType: string;
+    agendaURL: string;
+    agendaPacketURL: string;
+    summaryURL: string;
+    minutesURL: string;
+  } = {
     id: 1,
     meetingTime: "",
+    meetingType: "",
+    agendaURL: "",
+    agendaPacketURL: "",
+    summaryURL: "",
+    minutesURL: "",
   };
 
   const allMeetings = await prisma.$queryRaw<
@@ -32,18 +70,49 @@ export const getStaticProps: GetStaticProps = async () => {
   >`SELECT * FROM meetings WHERE meetingTime >= NOW() ORDER BY meetingTime ASC LIMIT 1;`;
 
   allMeetings.forEach((meeting) => {
-    if (meeting.meetingTime != null) {
+    if (
+      meeting.meetingTime != null &&
+      meeting.meetingType != null &&
+      meeting.agendaURL != null &&
+      meeting.agendaPacketURL != null &&
+      meeting.summaryURL != null &&
+      meeting.minutesURL != null
+    ) {
+      const meetingTimeFormatted = meeting.meetingTime.toLocaleDateString(
+        "en-US",
+        dateOptions
+      );
       queriedMeetings.push({
         id: meeting.meetingID,
-        meetingTime: meeting.meetingTime.toString(),
+        meetingTime: meetingTimeFormatted,
+        meetingType: meeting.meetingType,
+        agendaURL: meeting.agendaURL,
+        agendaPacketURL: meeting.agendaPacketURL,
+        summaryURL: meeting.summaryURL,
+        minutesURL: meeting.minutesURL,
       });
     }
   });
 
   if (upcomingMeeting[0]) {
     queryUpcoming.id = upcomingMeeting[0].meetingID;
-    if (upcomingMeeting[0].meetingTime != null) {
-      queryUpcoming.meetingTime = upcomingMeeting[0].meetingTime.toString();
+    if (
+      upcomingMeeting[0].meetingTime != null &&
+      upcomingMeeting[0].meetingType != null &&
+      upcomingMeeting[0].meetingType != null &&
+      upcomingMeeting[0].agendaURL != null &&
+      upcomingMeeting[0].agendaPacketURL != null &&
+      upcomingMeeting[0].summaryURL != null &&
+      upcomingMeeting[0].minutesURL != null
+    ) {
+      const meetingTimeFormatted =
+        upcomingMeeting[0].meetingTime.toLocaleDateString("en-US", dateOptions);
+      queryUpcoming.meetingTime = meetingTimeFormatted;
+      queryUpcoming.meetingType = upcomingMeeting[0].meetingType;
+      queryUpcoming.agendaURL = upcomingMeeting[0].agendaURL;
+      queryUpcoming.agendaPacketURL = upcomingMeeting[0].agendaPacketURL;
+      queryUpcoming.summaryURL = upcomingMeeting[0].summaryURL;
+      queryUpcoming.minutesURL = upcomingMeeting[0].minutesURL;
     }
   }
 
@@ -66,36 +135,173 @@ const Home: NextPage<HomeProps> = ({ queriedMeetings, queryUpcoming }) => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <h1 className="text-center text-7xl font-bold">Upcoming Meeting:</h1>
-        <div>
-          <Link
-            className="flex-column"
-            href={`/meetings/${queryUpcoming.id}`}
-            key={queryUpcoming.id}
-          >
-            <h1 className="text-center text-2xl">City Council Meeting</h1>
-            <h1 className="text-center text-2xl">
-              {queryUpcoming.meetingTime}
-            </h1>
-          </Link>
-        </div>
-      </div>
-      <Link href={"/"} className="text-3xl">
-        Meetings
-      </Link>
-      <div>
-        {queriedMeetings.map((meeting) => (
-          <div key={meeting.id}>
+      <div className="">
+        {/* Next Meeting Section */}
+        <div className="my-5 text-center">
+          <h1 className="my-5 font-bold text-5xl md:text-7xl 2xl:text-9xl">Next Meeting</h1>
+          <div>
             <Link
-              className="flex"
-              href={`/meetings/${meeting.id}`}
-              key={meeting.id}
+              className="flex-column"
+              href={`/meetings/${queryUpcoming.id}`}
+              key={queryUpcoming.id}
             >
-              {meeting.meetingTime}
+              <div className="text-2xl hover:underline flex flex-col">
+                <div className="">{queryUpcoming.meetingType}</div>
+                <div className="">{queryUpcoming.meetingTime}</div>
+              </div>
+              <div className="group relative mx-auto flex w-10 justify-center">
+                {/* Solid */}
+                <div className="hidden group-hover:block">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="h-10 w-10"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <div className="group-hover:hidden">
+                  {/* Outline */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="h-10 w-10 2xl:h-20 2xl:w-20"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </div>
+              </div>
             </Link>
           </div>
-        ))}
+        </div>
+        {/* Previous Meetings Title */}
+        <div className="max-w-full py-5 bg-primary-light">
+          <Link href={"/meetings"} className="">
+            <div className="mx-auto w-3/4 text-3xl font-bold 2xl:text-5xl 2xl:w-2/3">
+              Previous Meetings
+            </div>
+          </Link>
+          <div className="mx-auto w-3/4 text-sm 2xl:text-xl 2xl:w-2/3">Last Updated:</div>
+        </div>
+        {/* 10 Previous Meetings */}
+        <div className="">
+          {queriedMeetings.map((meeting, index) => (
+            <div
+              key={meeting.id}
+              className={index % 2 == 0 ? "" : "bg-primary-light"}
+            >
+              <div className="mx-auto px-5 py-5 md:px-0 md:mx-auto md:items-center md:justify-between md:w-3/4 md:flex md:flex-row 2xl:text-2xl 2xl:w-2/3">
+                <Link
+                  className=""
+                  href={`/meetings/${meeting.id}`}
+                  key={meeting.id}
+                >
+                  <div className="flex items-center text-center md:text-left">
+                    <div className="w-full">
+                      <div className="hover:underline">
+                        <div className="">{meeting.meetingType}</div>
+                        <div className="">{meeting.meetingTime}</div>
+                      </div>
+                    </div>
+                    <div className="group">
+                      {/* Solid SVG */}
+                      <div className="hidden group-hover:block">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="h-6 w-6 2xl:h-10 2xl:w-10"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <div className="group-hover:hidden">
+                        {/* Outline SVG */}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="h-6 w-6 2xl:h-10 2xl:w-10"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+                {/* Agenda, Agenda Packet, Minutes, and Summary URLs */}
+                <div className="block text-center space-x-3 pt-3 md:py-0 2xl:space-x-10">
+                  {meeting.agendaURL == "" ? null : (
+                    <Link
+                      href={
+                        `https://pattersonca.iqm2.com/Citizens/${meeting.agendaURL}`
+                      }
+                      className="hover:underline"
+                      target="_blank"
+                    >
+                      Agenda
+                    </Link>
+                  )}
+                  {meeting.agendaPacketURL == "" ? null : (
+                    <Link
+                      href={
+                        `https://pattersonca.iqm2.com/Citizens/${meeting.agendaPacketURL}`
+                      }
+                      className="hover:underline"
+                      target="_blank"
+                    >
+                      Agenda Packet
+                    </Link>
+                  )}
+                  {meeting.minutesURL == "" ? null : (
+                    <Link
+                      href={
+                        `https://pattersonca.iqm2.com/Citizens/${meeting.minutesURL}`
+                      }
+                      className="hover:underline"
+                      target="_blank"
+                    >
+                      Minutes
+                    </Link>
+                  )}
+                  {meeting.summaryURL == "" ? null : (
+                    <Link
+                      href={
+                        `https://pattersonca.iqm2.com/Citizens/${meeting.summaryURL}`
+                      }
+                      className="hover:underline"
+                      target="_blank"
+                    >
+                      Summary
+                    </Link>
+                  )}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );

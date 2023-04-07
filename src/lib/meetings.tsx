3e2686bab0b05e702e1be53f8531ba31fc36e293
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-04-07 15:11:08 -0700

    Individual meeting pages made pretty (#40)

--- src/lib/meetings.tsx
+++ src/lib/meetings.tsx
@@ -20,17 +20,52 @@ export async function getAllMeetingIds() {
 }
 
 export async function getMeetingData(meetingID: string) {
+  const dateOptions: Record<string, string | undefined> = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+  let meetingTimeFormatted = "Missing Date";
   const meetingIDInt = parseInt(meetingID);
   const agendaQuery = await prisma.agendas.findFirst({
     where: { meetingID: meetingIDInt },
     orderBy: { id: "desc" },
   });
+  const meetingQuery = await prisma.meetings.findFirst({
+    where: { meetingID: meetingIDInt },
+    orderBy: { id: "desc" },
+  });
   const agendaMD = agendaQuery?.agendaMD || "";
+  const meetingTime = meetingQuery?.meetingTime || "";
+  const meetingType = meetingQuery?.meetingType || "";
+  const meetingLink = meetingQuery?.meetingLink || "";
+  const boardType = meetingQuery?.boardType || "";
+  const agendaURL = meetingQuery?.agendaURL || "";
+  const agendaPacketURL = meetingQuery?.agendaPacketURL || "";
+  const summaryURL = meetingQuery?.summaryURL || "";
+  const minutesURL = meetingQuery?.minutesURL || "";
+
+  if (meetingTime != null && meetingTime != "") {
+    meetingTimeFormatted = meetingTime.toLocaleDateString(
+      "en-US",
+      dateOptions
+    );
+  }
 
   const processedContent = await remark().use(html).process(agendaMD);
   const contentHtml = processedContent.toString();
   return {
     meetingID,
     contentHtml,
+    meetingTimeFormatted,
+    meetingType,
+    meetingLink,
+    boardType,
+    agendaURL,
+    agendaPacketURL,
+    summaryURL,
+    minutesURL,
   };
 }
--- src/pages/meetings/[meetingID].tsx
+++ src/pages/meetings/[meetingID].tsx
@@ -1,19 +1,59 @@
+import Link from "next/link";
 import { getAllMeetingIds, getMeetingData } from "~/lib/meetings";
 
 interface MeetingData {
   meetingID: string;
   contentHtml: string;
+  meetingTimeFormatted: string;
+  meetingType: string;
+  meetingLink: string;
+  boardType: string;
+  agendaURL: string;
+  agendaPacketURL: string;
+  summaryURL: string;
+  minutesURL: string;
 }
 
 export default function Meeting({ meetingData }: { meetingData: MeetingData }) {
   return (
     <>
-      {meetingData.meetingID}
-      <br />
-      <article className="prose lg:prose-xl w-2/3 mx-auto">
+      <div className="flex flex-col items-center py-5">
+        <div className="text-3xl font-bold md:text-5xl">
+          {meetingData.boardType}
+        </div>
+        <div className="text-xl md:text-3xl">{meetingData.meetingType}</div>
+        <div className="text-xl md:text-3xl">
+          {meetingData.meetingTimeFormatted}
+        </div>
+      </div>
+      <div className="flex flex-col items-center space-x-5 space-y-3 bg-primary-light py-3 md:flex-row md:justify-center md:space-y-0">
+        <div className="hover:underline">
+          <Link href={meetingData.meetingLink}>Original Page</Link>
+        </div>
+        <div className="hover:underline">
+          <Link href={`https://pattersonca.iqm2.com/Citizens/${meetingData.agendaURL}`}>
+            Agenda
+          </Link>
+        </div>
+        <div className="hover:underline">
+          <Link href={`https://pattersonca.iqm2.com/Citizens/${meetingData.agendaPacketURL}`}>
+            Agenda Packet
+          </Link>
+        </div>
+        <div className="hover:underline">
+          <Link href={`https://pattersonca.iqm2.com/Citizens/${meetingData.summaryURL}`}>
+            Summary
+          </Link>
+        </div>
+        <div className="hover:underline">
+          <Link href={`https://pattersonca.iqm2.com/Citizens/${meetingData.minutesURL}`}>
+            Minutes
+          </Link>
+        </div>
+      </div>
+      <article className="prose mx-auto px-5 lg:prose-xl md:w-2/3 md:px-0">
         <div dangerouslySetInnerHTML={{ __html: meetingData.contentHtml }} />
       </article>
-      
     </>
   );
 }
@@ -26,7 +66,11 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({ params }: { params: { meetingID: string } }) {  
+export async function getStaticProps({
+  params,
+}: {
+  params: { meetingID: string };
+}) {
   const meetingData = await getMeetingData(params.meetingID);
   return {
     props: {

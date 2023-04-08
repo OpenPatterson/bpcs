8174cbbdde9674f95a75391dbe65e66f4316539f
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-04-08 11:54:19 -0700

    Broke out arrow SVG into component

--- src/components/nextMeeting.tsx
+++ src/components/nextMeeting.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import { ArrowIcon } from "~/components/arrowSVG";
 
 type Props = {
   id: number;
@@ -7,7 +8,11 @@ type Props = {
   meetingTime: string;
 };
 
-export const NextMeeting: React.FC<Props> = ({ id, meetingType, meetingTime }) => {
+export const NextMeeting: React.FC<Props> = ({
+  id,
+  meetingType,
+  meetingTime,
+}) => {
   return (
     <>
       <div className="my-5 text-center">
@@ -21,38 +26,7 @@ export const NextMeeting: React.FC<Props> = ({ id, meetingType, meetingTime }) =
               <div className="">{meetingTime}</div>
             </div>
             <div className="group relative mx-auto flex w-10 justify-center">
-              {/* Solid */}
-              <div className="hidden group-hover:block">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="h-10 w-10 2xl:h-20 2xl:w-20"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </div>
-              <div className="group-hover:hidden">
-                {/* Outline */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="h-10 w-10 2xl:h-20 2xl:w-20"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </div>
+              <ArrowIcon size={"h-10 w-10 2xl:h-20 2xl:w-20"} />
             </div>
           </Link>
         </div>
